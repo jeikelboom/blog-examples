fe522719fa5d4cf5bd65ce903aaadc9cfd63d97d
johaneikelboom <johan@jeikelboom.eu>
2021-09-01 20:11:24 +0200

    example Function concepts

--- HaskellSnippets/test/ConceptsSpec.hs
+++ HaskellSnippets/test/ConceptsSpec.hs
@@ -9,9 +9,8 @@ conceptsSpec = do
   
 tests = TestList [test1, test2]  
 
-
 test1 :: Test
-test1 = TestCase(assertEqual "No commute" false ((g . f . lek) Unit))
+test1 = TestCase(assertEqual "No commute g.f" false ((g . f . lek) Unit))
 
 test2 :: Test
-test2 = TestCase(assertEqual "No commute" false ((h . lek) Unit))
+test2 = TestCase(assertEqual "No commute h" false ((h . lek) Unit))
