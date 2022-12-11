3101b92d308a09ad3d935a07c819053dd408960f
johaneikelboom <johan@jeikelboom.eu>
2022-12-11 14:58:43 +0100

    update

---
+++ drawing/src/alarm.js
@@ -0,0 +1,9 @@
+function alarm() {
+    alert("The reactor is about to blow up.")
+    console.log("alarm raised")
+}
+
+console.log("begin execution")
+alarm()
+console.log("end execution")
+
--- kotlinSnippets/example/src/main/kotlin/fp/concepts/GCD.kt
+++
@@ -1,9 +0,0 @@
-package fp.concepts
-
-class GCD {
-
-    companion object{
-        fun gcd(a: Int, b: Int): Int = if (b==0) a else gcd(b, a % b)
-
-    }
-}
